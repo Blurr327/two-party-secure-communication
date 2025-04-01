a1b64d127250dd8d4a011919d318068651ce9585
Loussine <loussine.bas@etu.u-paris.fr>
2025-04-01 10:30:04 +0200

    Fonctions pour trouver un générateur de Zp*

--- elGamal.py
+++ elGamal.py
@@ -39,16 +39,10 @@ def gen_prime(bits: int) -> int:
 #Trouver un generateur de Zp* on veut que que g le generateur ait un ordre egal à p-1
 
 #Pollard Rho algorithm
-def prime_factors(n :int)->int:
-
-    # no prime divisor for 1 
-    if (n == 1):
-        return n
-
-    # even number : one of the divisors is 2 
-    if (n % 2 == 0):
+def pollard_rho(n: int) -> int :
+    #Cas n paire
+    if n % 2 == 0:
         return 2
-
     # we will pick from the range [2, N) 
     x = (random.randint(0, 2) % (n - 2))
     y = x
@@ -60,9 +54,7 @@ def prime_factors(n :int)->int:
 
     # Initialize candidate divisor (or result) 
     d = 1
-
-    # until the prime factor isn't obtained.
-    # If n is prime, return n 
+    
     while (d == 1):
     
         # Tortoise Move: x(i+1) = f(x(i)) 
@@ -78,16 +70,93 @@ def prime_factors(n :int)->int:
         # retry if the algorithm fails to find prime factor
         # with chosen x and c 
         if (d == n):
-            return prime_factors(n)
+            return pollard_rho(n)
     
     return d
+    
+#On sait que la taille de la liste doit faire p-1 pour generer Zp*
+def remove_duplicates(lst):
+    seen = set()
+    return [x for x in lst if not (x in seen or seen.add(x))]
 
-print(prime_factors(gen_prime(10)))
+    
+def prime_factors(n :int)-> list[int]:
+    ret=[]
+    # no prime divisor for 1 
+    if (n == 1):
+        return ret
+
+    # even number : one of the divisors is 2 
+    while n % 2 == 0:
+        ret.append(2)
+        n //= 2
+
+    if is_prime_miller_rabin(n):
+        ret.append(n)
+        return ret
+
+    while n > 1:
+        if is_prime_miller_rabin(n):
+            ret.append(n)
+            break
+        
+        factor = pollard_rho(n)
+        while n % factor == 0:
+            ret.append(factor)
+            n//=factor
+    return remove_duplicates(sorted(ret))
 
 
+a=gen_prime(10)-1
+print(a)
+print(prime_factors(a))
+
+
+def gen(n: int, p: int) -> list[int]:
+    return [pow(n, i, p) for i in range(1,p)]
+
+#slide 139 à voir 
+
+def is_gen(n: int, p: int) -> bool:
+    if len(remove_duplicates(gen(n,p))) == p-1:
+        return True
+    return False
+
+def generators(p : int ) -> list[int]:
+    ret=[]
+    factors=prime_factors(p-1)
+    
+    for i in range(len(factors)):
+        if is_gen(factors[i],p):
+            ret.append(factors[i])
+    return ret
+
+print(generators(13))
+print(gen(2,13))
+print(is_gen(2,13))
+    
+
+""" 
+print(gen(2,13))
+print(is_gen(2,13))
+print(is_gen(2,12))
+
+p=gen_prime(10)
+g=prime_factors(p-1)
+print(gen(g,p))
+print(is_gen(g,p))
 """
+
+    
+
+def prime_fact(n : int):
+    if is_prime_miller_rabin(n):
+        return n
+    
+
+# Slide 145 cours trouver generateur dans Zp*
 def find_generator(p: int) -> int:
     if not is_prime_miller_rabin(p):
         raise ValueError("p doit être un nombre premier")
     
-    q=p-1"""
+    q=p-1
